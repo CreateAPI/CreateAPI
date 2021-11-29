f29ad9e6e5f2e728fe786e17953ee1281f9f8f11
kean <grebenyuk.alexander@gmail.com>
2021-11-29 15:40:50 -0500

    Fix AnyJSON generated every time

---
+++ Scripts/test.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -eo pipefail
+
+scheme="Nuke"
+
+while getopts "s:d:" opt; do
+    case $opt in
+    	s) scheme=${OPTARG};;
+        d) destinations+=("$OPTARG");;
+        #...
+    esac
+done
+shift $((OPTIND -1))
+
+echo "scheme = ${scheme}"
+echo "destinations = ${destinations[@]}"
+
+xcodebuild -version
+
+xcodebuild build-for-testing -scheme "$scheme" -destination "${destinations[0]}"
+
+for destination in "${destinations[@]}";
+do
+	echo "\nRunning tests for destination: $destination"
+	xcodebuild test-without-building -scheme "$scheme" -destination "$destination"
+done
