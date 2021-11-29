4f744f27f4b7eff9583f526d8a2a6cc5ca23c9f5
kean <grebenyuk.alexander@gmail.com>
2021-11-29 14:24:42 -0500

    Add property tests

--- Tests/CreateAPITests/HelpersTests.swift
+++ Tests/CreateAPITests/HelpersTests.swift
@@ -11,44 +11,43 @@ final class HelpersTests: XCTestCase {
         XCTAssertEqual(TypeName("app").rawValue, "App")
         XCTAssertEqual(TypeName("app-manifests").rawValue, "AppManifests")
         XCTAssertEqual(TypeName("appManifests").rawValue, "AppManifests")
+        
+        XCTAssertEqual(TypeName("CamelCase").rawValue, "CamelCase")
+        XCTAssertEqual(TypeName("camelCase").rawValue, "CamelCase")
+        XCTAssertEqual(TypeName("nsw").rawValue, "Nsw")
+        XCTAssertEqual(TypeName("NSW").rawValue, "NSW")
+        XCTAssertEqual(TypeName("userURL").rawValue, "UserURL")
+        XCTAssertEqual(TypeName("url").rawValue, "URL")
+        XCTAssertEqual(TypeName("a-z").rawValue, "Az")
+        XCTAssertEqual(TypeName("A-Z").rawValue, "AZ")
+        XCTAssertEqual(PropertyName("123List").rawValue, "_123List")
+        XCTAssertEqual(TypeName("user_url").rawValue, "UserURL")
+        XCTAssertEqual(TypeName("snake_case").rawValue, "SnakeCase")
+        XCTAssertEqual(TypeName("SNAKE_CASE").rawValue, "SnakeCase")
+        XCTAssertEqual(TypeName("UserServiceDeviceHasAccessTo").rawValue, "UserServiceDeviceHasAccessTo")
     }
     
     func testPropertyName() {
-        XCTAssertEqual(PropertyName("app").rawValue, "app")
+        XCTAssertEqual(PropertyName("CamelCase").rawValue, "camelCase")
         XCTAssertEqual(PropertyName("app-manifests").rawValue, "appManifests")
         XCTAssertEqual(PropertyName("{code}").rawValue, "code")
         XCTAssertEqual(PropertyName("appManifests").rawValue, "appManifests")
         XCTAssertEqual(PropertyName("avatar_url").rawValue, "avatarURL")
         XCTAssertEqual(PropertyName("node_id").rawValue, "nodeID")
+        XCTAssertEqual(PropertyName("AppManifests").rawValue, "appManifests")
+
+        XCTAssertEqual(PropertyName("camelCase").rawValue, "camelCase")
+        XCTAssertEqual(PropertyName("NSW").rawValue, "nsw")
+        XCTAssertEqual(PropertyName("UserURL").rawValue, "userURL")
+        XCTAssertEqual(PropertyName("user_url").rawValue, "userURL")
+        XCTAssertEqual(PropertyName("url").rawValue, "url")
+        XCTAssertEqual(PropertyName("event-type").rawValue, "eventType")
+        XCTAssertEqual(PropertyName("a-z").rawValue, "aZ")
+        XCTAssertEqual(PropertyName("A-Z").rawValue, "aZ")
+        XCTAssertEqual(PropertyName("123List").rawValue, "_123List")
+        XCTAssertEqual(PropertyName("SNAKE_CASE").rawValue, "snakeCase")
+        XCTAssertEqual(PropertyName("snake_case").rawValue, "snakeCase")
+        XCTAssertEqual(PropertyName("UserServiceDeviceHasAccessTo").rawValue, "userServiceDeviceHasAccessTo")
+        XCTAssertEqual(PropertyName("UserService.getCustomerDevices").rawValue, "userServiceGetCustomerDevices")
     }
 }
-
-#warning("TODO:")
-//
-//try expect("CamelCase".lowerCamelCased()) == "camelCase"
-//try expect("camelCase".lowerCamelCased()) == "camelCase"
-//try expect("NSW".lowerCamelCased()) == "nsw"
-//try expect("UserURL".lowerCamelCased()) == "userURL"
-//try expect("user_url".lowerCamelCased()) == "userURL"
-//try expect("url".lowerCamelCased()) == "url"
-//try expect("event-type".lowerCamelCased()) == "eventType"
-//try expect("a-z".lowerCamelCased()) == "az"
-//try expect("A-Z".lowerCamelCased()) == "aZ"
-//try expect("SNAKE_CASE".lowerCamelCased()) == "snakeCase"
-//try expect("snake_case".lowerCamelCased()) == "snakeCase"
-//try expect("UserServiceDeviceHasAccessTo".lowerCamelCased()) == "userServiceDeviceHasAccessTo"
-//try expect("UserService.getCustomerDevices".lowerCamelCased()) == "userServiceGetCustomerDevices"
-//}
-//$0.it("upper camelcases") {
-//try expect("CamelCase".upperCamelCased()) == "CamelCase"
-//try expect("camelCase".upperCamelCased()) == "CamelCase"
-//try expect("nsw".upperCamelCased()) == "Nsw"
-//try expect("NSW".upperCamelCased()) == "NSW"
-//try expect("userURL".upperCamelCased()) == "UserURL"
-//try expect("url".upperCamelCased()) == "URL"
-//try expect("a-z".upperCamelCased()) == "Az"
-//try expect("A-Z".upperCamelCased()) == "AZ"
-//try expect("user_url".upperCamelCased()) == "UserURL"
-//try expect("snake_case".upperCamelCased()) == "SnakeCase"
-//try expect("SNAKE_CASE".upperCamelCased()) == "SnakeCase"
-//try expect("UserServiceDeviceHasAccessTo".upperCamelCased()) == "UserServiceDeviceHasAccessTo"
