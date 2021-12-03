1fcc874e7ec3c799d6b9679ea15d2c6ab8262175
kean <grebenyuk.alexander@gmail.com>
2021-12-02 22:17:46 -0500

    Reimpment test-generated

--- Scripts/test-generated.sh
+++ Scripts/test-generated.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
-// TODO: Reimplement (compile using Swift Package Manager)
+set -eo pipefail
 
-#set -eo pipefail
-#
-#for filename in ./Tests/CreateAPITests/Resources/Expected/*.txt; do
-#    cp -- "$filename" "${filename%.txt}.swift"
-#done
-#
-#for filename in ./Tests/CreateAPITests/Resources/Expected/*.swift; do
-#    swiftc $filename -o temp-bin
-#    rm temp-bin
-#done
-#
-#for filename in ./Tests/CreateAPITests/Resources/Expected/*.swift; do
-#    rm "$filename"
-#done
+for dir in ./Tests/CreateAPITests/Expected/*/; do
+    echo "Validating $dir"
+    cd $dir
+    ls
+    swift build
+    cd --
+done
