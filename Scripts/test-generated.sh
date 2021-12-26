491d92d0f7fe822d79c3ad78d71885dc6e0685d0
kean <grebenyuk.alexander@gmail.com>
2021-12-26 16:57:53 -0500

    Remove xcpretty

--- Scripts/test-generated.sh
+++ Scripts/test-generated.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 cd ./Tests
 xcodebuild clean
-xcodebuild build -scheme 'GeneratedPackages' -destination "OS=15.2,name=iPhone 13"  | xcpretty
+xcodebuild build -scheme 'GeneratedPackages' -destination "OS=15.2,name=iPhone 13"
