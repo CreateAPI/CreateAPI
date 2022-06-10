8911e9d319eee8496d9d7a348ec79ea373672cf1
Liam Nichols <liam.nichols.ln@gmail.com>
2022-06-10 15:24:50 +0200

    Replicate nullable + single allOf bug in schema. Write expected output manually

--- Tests/CreateAPITests/Expected/inlining-default/Sources/Entities.swift
+++ Tests/CreateAPITests/Expected/inlining-default/Sources/Entities.swift
@@ -26,22 +26,26 @@ public struct Item: Codable {
 public struct Package: Codable {
     public var weight: Int
     public var address: String
+    public var receivedBy: Person?
 
-    public init(weight: Int, address: String) {
+    public init(weight: Int, address: String, receivedBy: Person? = nil) {
         self.weight = weight
         self.address = address
+        self.receivedBy = receivedBy
     }
 
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: StringCodingKey.self)
         self.weight = try values.decode(Int.self, forKey: "weight")
         self.address = try values.decode(String.self, forKey: "address")
+        self.receivedBy = try values.decodeIfPresent(Person.self, forKey: "receivedBy")
     }
 
     public func encode(to encoder: Encoder) throws {
         var values = encoder.container(keyedBy: StringCodingKey.self)
         try values.encode(weight, forKey: "weight")
         try values.encode(address, forKey: "address")
+        try values.encodeIfPresent(receivedBy, forKey: "receivedBy")
     }
 }
 
