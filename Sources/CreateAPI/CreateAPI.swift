6f3564f4c0da627859564987cc3d6987f3241fd1
Liam Nichols <liam.nichols.ln@gmail.com>
2022-06-29 21:52:35 +0100

    Remove main.swift and mark CreateAPI as @main type directly

--- Sources/CreateAPI/CreateAPI.swift
+++ Sources/CreateAPI/CreateAPI.swift
@@ -3,17 +3,12 @@
 // Copyright (c) 2021-2022 Alexander Grebenyuk (github.com/kean).
 
 import ArgumentParser
-import OpenAPIKit30
-import Foundation
-import Yams
 
+@main
 struct CreateAPI: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "create-api",
         abstract: "A Swift command-line tool to auto-generate code for OpenAPI specs",
-        subcommands: [Generate.self])
-
-    init() { }
+        subcommands: [Generate.self]
+    )
 }
-
-CreateAPI.main()
